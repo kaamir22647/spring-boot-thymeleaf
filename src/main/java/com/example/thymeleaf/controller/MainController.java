2b603ee872e3564ef26df4a22a7e33e4b438bfff
kaamir22647 <kaamir22647@gmail.com>
2024-10-13 16:27:48 +0530

    first commit

---
+++ src/main/java/com/example/thymeleaf/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/home")
+	public String loadHome(Model model) {
+		model.addAttribute("name","Aamir");
+		return "home";
+	}
+}
